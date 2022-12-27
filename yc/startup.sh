e9c7bac11b7313ddbe5887b83b8b4d64b862b17d
Ivan Enzel <enzel@live.com>
2022-12-27 12:58:35 +0500

    Removing junk files

--- yc/startup.sh
+++
@@ -1,9 +0,0 @@
-#Bash file for lesson in Yandex Practicum (ycloud)
-#Uses in yc command for cloud-init module
-
-#!/bin/bash
-apt-get update
-apt-get install -y nginx
-service nginx start
-sed -i -- "s/nginx/Yandex Cloud - ${HOSTNAME}/" /var/www/html/index.nginx-debian.html
-EOF
